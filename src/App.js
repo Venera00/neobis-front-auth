1719cac72800b08e438bee932c794ebcb560dbe1
Venera <veneraomurzakkyzy@gmail.com>
2023-12-06 02:52:57 +0600

    Modify loggedIn page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/loggedin" element={<LoggedInPage />} />
         </Routes>
       </BrowserRouter>
--- src/pages/LoggedIn/LoggedInPage.js
+++ src/pages/LoggedIn/LoggedInPage.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 import mainImg from "../../assets/mainImg.jpg";
 import "./LoggedInPage.css";
 
 const LoggedInPage = () => {
   return (
-    <div className="loggedin-conatiner">
+    <div className="loggedin-container">
       <h2 className="loggedin-title">С возвращением!</h2>
       <p className="loggedin-subtitle">Lorby - твой личный репетитор</p>
       <img src={mainImg} />
+      <Link to="/login" className="loggedin-link">
+        Выйти
+      </Link>
     </div>
   );
 };
