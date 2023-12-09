2404c0680454e66b76ace0b3bdae1972a5df9709
Venera <veneraomurzakkyzy@gmail.com>
2023-12-09 07:10:51 +0600

    Add api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,1 +1,8 @@
-// const URL = "http://139.59.159.246:8000/api/schema/swagger-ui/";
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://139.59.159.246:8000/api/schema/swagger-ui/",
+  headers: {
+    "Content-Type": "application.json",
+  },
+});
